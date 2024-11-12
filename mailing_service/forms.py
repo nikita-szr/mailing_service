f491a2c9eb82ab4bff83eb3206308bbda5cb836a
nikitt <nikittaosipov@yandex.ru>
2024-11-13 01:27:37 +0400

    css стиль в шаблонах

--- mailing_service/forms.py
+++ mailing_service/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.utils import timezone
 from .models import CustomUser, Mailing, MailingMessage, MailingRecipient
 
 
